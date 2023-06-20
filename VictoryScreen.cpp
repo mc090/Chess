b2287d9b16b8d9a11a6844bd841b909a009aa09d
mc090 <venixis0@gmail.com>
2023-06-20 22:48:00 +0200

    drobna zmiana

--- VictoryScreen.cpp
+++ VictoryScreen.cpp
@@ -4,13 +4,13 @@ VictoryScreen::VictoryScreen(const gameResult& game_result)
 {
 	switch (game_result) {
 	case black_won:
-		_texture.loadFromFile("Textures/WhiteVictory.png");
+		_texture.loadFromFile("Textures/BlackVictory.png");
 		break;
 	case white_won:
 		_texture.loadFromFile("Textures/WhiteVictory.png");
 		break;
 	case tie:
-		_texture.loadFromFile("Textures/WhiteVictory.png");
+		_texture.loadFromFile("Textures/Draw.png");
 		break;
 	}
 	_sprite.setTexture(_texture);
